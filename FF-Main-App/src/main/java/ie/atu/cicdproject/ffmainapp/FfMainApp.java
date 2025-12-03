6a46aebaffb9dca75a8c34894f1b3a0785f9b92d
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-12-03 13:50:00 +0000

    Removed UserTesting

---
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FfMainApp.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdproject.ffmainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfMainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfMainApp.class, args);
+    }
+
+}
