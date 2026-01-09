c0e5b482caaa509c90da0fb6ab6ae0850696cb20
daraghkearney <g00438215@atu.ie>
2026-01-09 21:51:00 +0000

    Added DTO and Feign

--- FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FfMainApp.java
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FfMainApp.java
@@ -2,12 +2,12 @@ package ie.atu.cicdproject.ffmainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class FfMainApp {
-
     public static void main(String[] args) {
         SpringApplication.run(FfMainApp.class, args);
     }
-
 }
